1212c9eb8f2665df6ead67f4d837b322860e65fb
VarshaML <varsha.ml@tecnotree.com>
2025-08-05 16:44:25 +0530

    feat: day 2

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
-{
-  /* <div id="parent" >
-  <div id="child1" >
-  <h1>I am a child H1 tag!</h1>
-  <h2> I am a child H2 tag!</h2>
-  </div>
-  <div id="child2" >
-  <h1>I am a child H1 tag!</h1>
-  <h2> I am a child H2 tag!</h2>
-  </div>
-  </div> */
-}
-//how to render a nested html element multiple siblings in react
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1", key: "child1" }, [
     React.createElement("h1", { key: "child1" }, "I am a child H1 tag!"),
