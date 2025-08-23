03d1aa9794d3da3363fadcaec50ff9dddc0e1ac0
VarshaML <varsha.ml@tecnotree.com>
2025-08-23 16:11:26 +0530

    feat: Restaurant card (day4)

--- App.js
+++ App.js
@@ -1,29 +1,93 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import restaurants from "./restaurants.json";
+//now let's build a food ordering application using react
+// the components we need in pur mock design
+/* Header
+ * -  Logo
+ * - Nav Items
+ * Body
+ * - Search Bar
+ * - Restaurant Container
+ *  - Restaurant Card
+ *    - Image
+ *    - Name of Restaurant, Rating, price, cuisine, deleivery time
+ * Footer
+ * - Contact Us
+ * - About Us
+ * - Copyrights
+ */
 
-//React.createElement => Object => HTMLElement(render)
-
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-
-//JSX is not HTML in JS , JSX is HTML like syntax
-//JSX => React.createElement => Object => HTMLElement(render)
-const jsxHeading = <h1 id="heading">Namaste React-iuAEGFI</h1>;
-
-//React Element
-const headingElement = <h1 id="heading">Namaste React-headingElement</h1>;
-
-//React Component
-const Title = () => <h1 className="heading">Namaste React-title</h1>;
-//Component composition
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div id="container">
-      <Title />
-      <h1 id="heading">Namaste React-inside Heading component</h1>
+    <header className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+const ResturantCard = (props) => {
+  const {
+    resData: {
+      info: { name, cuisines, cloudinaryImageId, avgRating, sla, costForTwo },
+    },
+  } = props;
+  return (
+    <div className="rest-card">
+      <img
+        className="rest-img"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <div>
+        <div className="rest-heading">{name}</div>
+        <div className="rating">
+          <div>{avgRating} stars</div>
+          <div>{sla.slaString}</div>
+        </div>
+        <div className="cuisine">{cuisines.join(", ")}</div>
+      </div>
     </div>
   );
 };
-console.log({ HeadingComponent, headingElement });
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="rest-container">
+        {restaurants.restaurants.map((rest) => (
+          <ResturantCard resData={rest} key={rest.info.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
