adfe5703b7d9291e29c3bad45bc647a870183110
VarshaML <varsha.ml@tecnotree.com>
2025-08-24 15:17:43 +0530

    feat: api call and useEffect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,38 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { restList } from "../utils/constants";
 import ResturantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  //Local State variable - super powerful variable
-  // useState() Returns an array of stateful value, and a function to update it.
-  // so here we are destrcuturing the array returned by useState
-  // we can also write it as - const arr = useState(restList.restaurants);
-  // and then const [listOfRestaurants, setListOfRestaurants] = arr;
-  const [listOfRestaurants, setListOfRestaurants] = useState(
-    restList?.restaurants
-  );
-  const test = React.createElement("h1", { id: "title" }, "Namaste React");
-  console.log(<ResturantCard resData={restList.restaurants[0]} />, test);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //conditional rendering
+  //   if (listOfRestaurants.length === 0) {
+  //     //old practice
+  //     // return <h2>Loading...</h2>;
+  //     //shimmer ui - bets practice
+  //     return <Shimmer />;
+  //   }
+
   return (
     <div className="body">
-      <div className="search">Search</div>
-      <div>
-        <button
-          onClick={() => {
-            const filteredList = restList.restaurants.filter(
-              (rest) => rest.info.avgRating > 4.2
-            );
-            setListOfRestaurants(filteredList);
-          }}
-        >
-          Filter
-        </button>
-      </div>
-      <div className="rest-container">
-        {listOfRestaurants.map((rest) => (
-          <ResturantCard resData={rest} key={rest.info.id} />
-        ))}
-      </div>
+      {listOfRestaurants.length === 0 ? (
+        <Shimmer />
+      ) : (
+        <>
+          <div className="search">Search</div>
+          <div>
+            <button
+              onClick={() => {
+                const filteredList = restList.restaurants.filter(
+                  (rest) => rest.info.avgRating > 4.2
+                );
+                setListOfRestaurants(filteredList);
+              }}
+            >
+              Filter
+            </button>
+          </div>
+          <div className="rest-container">
+            {listOfRestaurants.map((rest) => (
+              <ResturantCard resData={rest} key={rest.info.id} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
