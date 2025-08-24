d32096044963f51d106a097c30184a96ba604a46
VarshaML <varsha.ml@tecnotree.com>
2025-08-24 13:09:46 +0530

    feat: useState and react fiber

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 
 const AppLayout = () => {
+  console.log(<Body />);
   return (
     <div className="app">
       <Header />
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,35 @@
+import React, { useState } from "react";
 import { restList } from "../utils/constants";
 import ResturantCard from "./RestaurantCard";
 
 const Body = () => {
+  //Local State variable - super powerful variable
+  // useState() Returns an array of stateful value, and a function to update it.
+  // so here we are destrcuturing the array returned by useState
+  // we can also write it as - const arr = useState(restList.restaurants);
+  // and then const [listOfRestaurants, setListOfRestaurants] = arr;
+  const [listOfRestaurants, setListOfRestaurants] = useState(
+    restList?.restaurants
+  );
+  const test = React.createElement("h1", { id: "title" }, "Namaste React");
+  console.log(<ResturantCard resData={restList.restaurants[0]} />, test);
   return (
     <div className="body">
       <div className="search">Search</div>
+      <div>
+        <button
+          onClick={() => {
+            const filteredList = restList.restaurants.filter(
+              (rest) => rest.info.avgRating > 4.2
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Filter
+        </button>
+      </div>
       <div className="rest-container">
-        {restList.restaurants.map((rest) => (
+        {listOfRestaurants.map((rest) => (
           <ResturantCard resData={rest} key={rest.info.id} />
         ))}
       </div>
