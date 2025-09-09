6e3abe7c917877d5a9af866012dd8abdf66a54e2
VarshaML <varsha.ml@tecnotree.com>
2025-09-09 16:23:52 +0530

    feat: react-router part 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Router, RouterProvider } from "react-router";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
   return (
@@ -12,6 +16,23 @@ const AppLayout = () => {
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//createBrowserRouter - used to create routes. it accepts an array of objects as an argument which represents the routes and contains path, element and errorElement properties
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
-root.render(<AppLayout />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// RouterProvider - used to render the routes
+root.render(<RouterProvider router={appRouter} />);
